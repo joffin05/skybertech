1d50aa34d77fc98b5b59e609ad1db647f6ee8258
Riyas Rawther <riyasrawther.in@gmail.com>
2020-03-12 10:01:44 +0530

    Addded Internal folder download, directoy setup, NGINX vhosts, osticket, osticket fixes e.t.c

--- intranet.sh
+++ intranet.sh
@@ -23,3 +23,60 @@ sudo apt-get install php7.2
 
 # Install needed modules for PHP
 sudo apt-get install php7.2-fpm php7.2-mysql php7.2-curl php7.2-gd php7.2-bz2 php7.2-mbstring php7.2-xml php7.2-zip
+
+# Install Git
+sudo apt install git
+git --version
+git config --global user.name "Riyas Rawther"
+git config --global user.email "riyasrawther.in@gmail.com"
+# Download the full folder
+cd /tmp
+https://github.com/riyas-rawther/intranet_apps_lemp.git
+
+cd intranet_apps_lemp
+
+# Create required folders
+
+mkdir -p -v /var/www/internal
+# mkdir -p -v /var/www/osticket
+mkdir -p -v /var/www/moodle
+mkdir -p -v /var/www/seeddms
+mkdir -p -v /var/www/itdb
+
+# Move Internal folder to /var/www/internal
+
+mv internal * /var/www/internal
+
+#Restore Internal DB from Dump
+
+mysql internal < /var/www/internal/sql.sql
+
+# Install OS Ticket for github
+cd /tmp
+git clone https://github.com/osTicket/osTicket
+cd osTicket
+php manage.php deploy --setup /var/www/osticket/
+
+# Fix OsTicket AJAX issue with NGINX
+
+
+# Move NGINX host files to sites Available
+
+mv /tmp/intranet_apps_lemp/nginx_vhosts * /etc/nginx/sites-available
+
+# Create NGINX links
+
+sudo ln -s /etc/nginx/sites-available/internal.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/osticket.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/moodle.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/seeddms.conf /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/itdb.conf /etc/nginx/sites-enabled/
+
+
+#Remove Default NGINX File
+sudo rm /etc/nginx/sites-available/default
+# test Nginx
+sudo nginx-t
+# Restart NGINX
+
+sudo systemctl restart nginx.service
